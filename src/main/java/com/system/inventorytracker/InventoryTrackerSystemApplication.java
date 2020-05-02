33e2531bc49c47e68d52f58eba76c5388db37707
Julius Braham Neria <juliusbraham.neria@gmail.com>
2020-05-02 15:51:38 +0800

    first commit

---
+++ src/main/java/com/system/inventorytracker/InventoryTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.inventorytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/inventorytracker/InventoryTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.inventorytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
